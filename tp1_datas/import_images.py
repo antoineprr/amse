3e9604f82641832d41543fa6d6383f65442a8c92
Antoine Poirier <antoine.poirier@etu.imt-nord-europe.fr>
2025-02-12 17:50:29 +0100

    feat: code python pour récupération des stats et images des joueurs

---
+++ tp1_datas/import_images.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+
+# Fonction pour récupérer l'image d'un joueur
+def get_player_image(player_id):
+    url = f'https://www.nba.com/stats/player/{player_id}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Trouver la balise img avec les classes spécifiques
+    img_tag = soup.find('img', {'class': 'PlayerImage_image__wH_YX PlayerSummary_playerImage__sysif'})
+    if img_tag:
+        img_src = img_tag['src']
+        return img_src
+    return None
+
+# Fonction pour télécharger et sauvegarder l'image
+def download_image(url, player_name, save_directory):
+    if not url:
+        print(f"No image URL for player {player_name}")
+        return
+    
+    # Créer le répertoire s'il n'existe pas
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
+    
+    # Obtenir le nom du fichier à partir de l'URL
+    file_name = f"{player_name.replace(' ', '_')}.png"  # Remplace les espaces par des underscores
+    file_path = os.path.join(save_directory, file_name)
+    
+    # Télécharger l'image
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+        print(f"Image saved for player {player_name} at {file_path}")
+    else:
+        print(f"Failed to download image for player {player_name}")
+
+# Charger les données JSON
+with open('top_20_ppg_2023_24_formatted.json', 'r') as file:
+    players_data = json.load(file)
+
+# Répertoire pour sauvegarder les images
+save_directory = 'player_images'
+
+# Itérer sur chaque joueur pour récupérer et télécharger l'image
+for player in players_data:
+    player_id = player['PLAYER_ID']
+    player_name = player['PLAYER']
+    image_url = get_player_image(player_id)
+    download_image(image_url, player_name, save_directory)
---
+++ tp1_datas/recup_data_nba_json.py
@@ -0,0 +1,21 @@
+from nba_api.stats.endpoints import leagueleaders
+import pandas as pd
+import json
+
+# Récupérer les leaders de la ligue pour la saison 2023-24
+leaders = leagueleaders.LeagueLeaders(season='2023-24')
+
+# Obtenir les données sous forme de DataFrame
+leaders_df = leaders.get_data_frames()[0]
+
+# Sélectionner les 20 premiers joueurs
+top_20_ppg = leaders_df.head(20)
+
+# Convertir le DataFrame en dictionnaire
+top_20_ppg_dict = top_20_ppg.to_dict(orient='records')
+
+# Exporter les données au format JSON, formaté correctement
+with open("top_20_ppg_2023_24_formatted.json", "w") as json_file:
+    json.dump(top_20_ppg_dict, json_file, indent=4)
+
+print("\nDonnées exportées au format JSON correctement formaté dans le fichier 'top_20_ppg_2023_24_formatted.json'")
